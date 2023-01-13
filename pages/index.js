4c196cdd56209947d545a7876185c28c1a8f13a6
Lipton Barua <liptonbarua274@gmail.com>
2023-01-13 20:39:53 +0600

    project

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,18 @@ import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 import Pricing from '../Components/Pricing/Pricing'
 import Footer from '../Components/Footer/Footer'
+import Header from '../Components/Header/Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
+    <div className='bg-gradient-to-r from-indigo-200 via-purple-10'>
+    <div className='lg:max-w-[1140px] mx-auto'>
+    <Header></Header>
       <Pricing></Pricing>
-       <Footer></Footer>
-    </>
+      <Footer></Footer>
+    </div>
+    </div>
   )
 }
