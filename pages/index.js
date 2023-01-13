1603a1b5a1c6c0b4c14082a299f2390553313bb2
RHN007 <rakibul.nayon@outlook.com>
2023-01-13 19:47:40 +0600

    adding Border

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,14 @@ import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 import Pricing from '../Components/Pricing/Pricing'
 import Footer from '../Components/Footer/Footer'
+import Banner from '../Components/Banner/Banner'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
+      <Banner></Banner>
       <Pricing></Pricing>
        <Footer></Footer>
     </>
