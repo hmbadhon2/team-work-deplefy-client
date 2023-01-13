49a548515cfe4a94a4a493154de8ecccb64091ee
Lipton Barua <liptonbarua274@gmail.com>
2023-01-14 01:02:18 +0600

    project

--- Components/Header/Header.js
+++ Components/Header/Header.js
@@ -2,50 +2,42 @@ import Image from "next/image";
 import { useState } from "react";
 
 const Header = () => {
-	const [open, setOpen] = useState(false)
-	return (
-		<div>
-			<header className="p-4 dark:bg-gray-800 dark:text-gray-100">
-				<div className="container flex justify-between h-16 mx-auto">
-					<div className="flex">
-						<a rel="noopener noreferrer" href="#" aria-label="Back to homepage" className="flex items-center p-2">
-							<Image src="/Perfect_unique_attractive_stylish_geometric_tech_PD_DP_P_D_initial_based_letter_icon_logo.-removebg-preview.png" width={150} height={150}></Image>
-						</a>
-						<ul className="items-stretch space-x-3 blog hidden md:flex">
-							<li className="flex">
-								<a rel="noopener noreferrer" href="#" className="flex items-center px-4 -mb-1 border-b-2 dark:border-transparent">Process</a>
-							</li>
-							<li className="flex">
-								<a rel="noopener noreferrer" href="#" className="flex items-center px-4 -mb-1 border-b-2 dark:border-transparent dark:text-violet-400 dark:border-violet-400">Resources</a>
-							</li>
-							<li className="flex">
-								<a rel="noopener noreferrer" href="#" className="flex items-center px-4 -mb-1 border-b-2 dark:border-transparent">Pricing</a>
-							</li>
-						</ul>
-					</div>
-					<div className="items-center flex-shrink-0 hidden lg:flex">
-						<button className="btn btn-outline btn-info self-center px-6 py-3 rounded-3xl mr-5">Sign in</button>
-						<button className="btn btn-outline btn-info self-center px-6 py-3 font-semibold rounded-3xl">Sign up</button>
-					</div>
-			      <div onClick={()=>setOpen(!open)} >
-				  {
-						open?<button className="p-4 lg:hidden">
-						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-							<path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-						</svg>
-					</button>
-					:
-					<button className="p-4 lg:hidden">
-					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6 dark:text-gray-100">
-						<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
-					</svg>
-				</button>
-					}
-				  </div>
-				</div>
-			</header>
+	const[open, setOpen]=useState(false)
+    return (
+        <div>
+            <header className="p-4 dark:bg-gray-800 dark:text-gray-100">
+			<div className="container flex justify-between h-16 mx-auto">
+		<div className="flex">
+			<a rel="noopener noreferrer" href="#" aria-label="Back to homepage" className="flex items-center p-2">
+			<Image src="/Perfect_unique_attractive_stylish_geometric_tech_PD_DP_P_D_initial_based_letter_icon_logo.-removebg-preview.png" width={150} height={150}></Image> 
+			</a>
+			<ul className="items-stretch space-x-3 blog hidden md:flex">
+				<li className="flex">
+					<a rel="noopener noreferrer" href="#" className="flex items-center px-4 -mb-1 border-b-2 dark:border-transparent">Process</a>
+				</li>
+				<li className="flex">
+					<a rel="noopener noreferrer" href="#" className="flex items-center px-4 -mb-1 border-b-2 dark:border-transparent dark:text-violet-400 dark:border-violet-400">Resources</a>
+				</li>
+				<li className="flex">
+					<a rel="noopener noreferrer" href="#" className="flex items-center px-4 -mb-1 border-b-2 dark:border-transparent">Pricing</a>
+				</li>
+			</ul>
 		</div>
-	);
+		<div className="items-center flex-shrink-0 hidden lg:flex">
+		<button className="btn btn-outline btn-info self-center px-6 py-3 rounded-3xl mr-5">Sign in</button>
+		<button className="btn btn-outline btn-info self-center px-6 py-3 font-semibold rounded-3xl">Sign up</button>
+		</div>
+		<button className="p-4 lg:hidden">
+			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6 dark:text-gray-100">
+				<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
+			</svg>
+		</button>
+
+		
+	</div>
+</header>
+        </div>
+    );
 };
 
 export default Header;
