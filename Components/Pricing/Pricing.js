c817b2059618cb0d7b93a927840c31e5fafa5c0e
Lipton Barua <liptonbarua274@gmail.com>
2023-01-13 12:09:46 +0600

    pricing plan complete

--- Components/Pricing/Pricing.js
+++ Components/Pricing/Pricing.js
@@ -50,7 +50,7 @@ const Pricing = () => {
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="flex-shrink-0 w-6 h-6">
                                             <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
                                         </svg>
-                                        <span>Slack integrationnnnn</span>
+                                        <span>Slack integration</span>
                                     </li>
                                 </ul>
                                 <button className="btn btn-outline btn-info">Add To Cart</button>
