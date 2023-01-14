15fe0f86ca5d8d5c895a1d01e8c93ef4a97297c6
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2023-01-14 14:19:02 +0600

    done section

--- Components/Subscribe/Subscribe.js
+++ Components/Subscribe/Subscribe.js
@@ -4,13 +4,13 @@ import Image from "next/image";
 const Subscribe = () => {
     return (
         <div>
-             <div className="flex flex-col items-center mb-14 ">
+             <div className="flex flex-col items-center mb-20 ">
              <Image src="/Perfect_unique_attractive_stylish_geometric_tech_PD_DP_P_D_initial_based_letter_icon_logo.-removebg-preview.png" width={200} height={100}></Image>
 
 
-                <h1 className="font-sans">Stay up to date with all Jamstack & Deplipy news</h1>
+                <h1 className="font-sans font-normal">Stay up to date with all Jamstack & Deplipy news</h1>
                 <div className=" flex items-center mt-4">
-                <input type="text" placeholder="Email Address" className="input input-bordered mr-1" />
+                <input type="email" placeholder="Email Address" className="input input-bordered mr-1" />
                     <button className="btn btn-outline btn-info ">Subscribe</button>
                 </div>
              </div>
