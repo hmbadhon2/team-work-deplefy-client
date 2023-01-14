2f7a017a6a6859415d06ef206fd92cf8e0b83ca0
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2023-01-14 13:28:49 +0600

    add

---
+++ Components/Subscribe/Subcribe.js
@@ -0,0 +1,13 @@
+
+const Subcribe = () => {
+    return (
+        <div>
+            <h1>subscribe</h1>
+            <h1>input and button</h1>
+        </div>
+    );
+};
+
+
+export default Subcribe;
+
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 import Pricing from '../Components/Pricing/Pricing'
 import Footer from '../Components/Footer/Footer'
+import Subcribe from '../Components/Subscribe/Subcribe'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,6 +13,7 @@ export default function Home() {
     <>
       <Pricing></Pricing>
        <Footer></Footer>
+       
     </>
   )
 }
