a346d5cded933a822984d9e4d9ae261fa7a10a4a
Lipton Barua <liptonbarua274@gmail.com>
2023-01-15 01:58:38 +0600

    project

--- Components/Banner/Banner.js
+++ Components/Banner/Banner.js
@@ -4,7 +4,7 @@ import Typewriter from 'typewriter-effect';
 const Banner = () => {
     return (
         <div className="hero">
-            <div className="hero-content flex-col lg:flex-row-reverse">
+            <div className="hero-content flex-col md:flex-row-reverse">
             <Image src="/remove.png" width={800} height={800}></Image>
                 <div>
                     <div className="text-4xl md:text-4xl lg:text-5xl font-bold text-pink-800">
@@ -17,7 +17,7 @@ const Banner = () => {
                         }}
                     />
                     </div>
-                    <h1 ></h1>
+                    
                     <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
                     <button className="btn btn-outline text-white bg-pink-800 self-center px-6 py-3 rounded-3xl mr-5">Get Started</button>
                 </div>
