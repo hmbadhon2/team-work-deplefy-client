3797a929ee3813b1c45d52fcc9fa1d1f13e7ecf1
Tamannarahman <tamannarahmanmridha@gmail.com>
2023-01-15 16:39:11 +0600

    done

--- pages/404.js
+++ pages/404.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 
 const Error = () => {
@@ -7,6 +8,11 @@ const Error = () => {
             <Image src='/404 page.jpg' width={1440} height={50}>
 
             </Image>
+            <Link href='/'>
+                <button className="btn btn-primary">
+                    Back to Home
+                </button>
+            </Link>
         </div>
     );
 };
