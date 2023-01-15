7a19078d816a3dceec5b87267b771791e3b807b6
Tamannarahman <tamannarahmanmridha@gmail.com>
2023-01-15 16:13:50 +0600

    404 page create

---
+++ pages/404.js
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+
+const Error = () => {
+    return (
+        <div>
+            <Image src='/404 page.jpg' width={1440} height={50}>
+
+            </Image>
+        </div>
+    );
+};
+
+export default Error;
