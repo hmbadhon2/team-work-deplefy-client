c4319d2ae490153cdb130f7802991c0c3a9cd3ec
Lipton Barua <liptonbarua274@gmail.com>
2023-01-16 01:56:38 +0600

    project

--- Components/Header/Header.js
+++ Components/Header/Header.js
@@ -5,45 +5,48 @@ const Header = () => {
 	const [open, setOpen] = useState(false)
 	return (
 		<div>
-			<header className="py-4 dark:bg-gray-800 dark:text-gray-100">
-				<div className="container flex justify-between h-16">
-					<div className="flex">
-						<a rel="noopener noreferrer" href="#" aria-label="Back to homepage" className="flex items-center p-2">
-							<Image src="/removebg-preview.png" width={150} height={150}></Image>
-						</a>
-						<ul className="items-stretch  blog hidden md:flex">
+			<div className="navbar">
+				<div className="navbar-start">
+					<div className="w-14 rounded">
+						<img src="https://i.ibb.co/5GgkG1j/removebg-preview.png" />
+					</div>
+
+					<div className="ml-4">
+						<ul className="hidden md:flex">
 							<li className="flex">
-								<a rel="noopener noreferrer" href="#" className="flex items-center px-4 -mb-1 font-bold text-gray-400 ">Process</a>
+								<a rel="noopener noreferrer" href="#" className="manu-item flex items-center px-4 -mb-1 font-bold text-gray-400 ">Process</a>
 							</li>
 							<li className="flex">
-								<a rel="noopener noreferrer" href="#" className="flex items-center px-4 -mb-1 font-bold text-gray-400">Resources</a>
+								<a rel="noopener noreferrer" href="#" className="manu-item flex items-center px-4 -mb-1 font-bold text-gray-400">Resources</a>
 							</li>
 							<li className="flex">
-								<a rel="noopener noreferrer" href="#" className="flex items-center px-4 -mb-1 font-bold text-gray-400 ">Pricing</a>
+								<a rel="noopener noreferrer" href="#" className="manu-item flex items-center px-4 -mb-1 font-bold text-gray-400 ">Pricing</a>
 							</li>
 						</ul>
 					</div>
+				</div>
+
+				<div className="navbar-end">
 					<div className="items-center flex-shrink-0 hidden lg:flex">
 						<button className=" manu-button px-5 py-2 rounded-3xl mr-5">Login</button>
 						<button className=" manu-button px-5 py-2  rounded-3xl">Sign up</button>
 					</div>
-				
-					<div className="dropdown dropdown-bottom dropdown-end lg:hidden">
-						<label tabIndex={0} className="m-1"><button className="p-4">
-							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6 dark:text-gray-100">
-								<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
-							</svg>
-						</button></label>
-						<ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
-							<li><a>Process</a></li>
-							<li><a>Resources</a></li>
-							<li><a>Pricing</a></li>
-							<button className="manu-button  py-3 rounded-3xl mb-5">Login</button>
-						    <button className="manu-button py-3  rounded-3xl">Sign up</button>
-						</ul>
-					</div>
 				</div>
-			</header>
+				<div className="dropdown dropdown-bottom dropdown-end lg:hidden">
+					<label tabIndex={0} className="m-1"><button className="p-4">
+						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6 dark:text-gray-100">
+							<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
+						</svg>
+					</button></label>
+					<ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+						<li><a className="manu-item">Process</a></li>
+						<li><a className="manu-item">Resources</a></li>
+						<li><a className="manu-item">Pricing</a></li>
+						<button className="manu-button  py-3 rounded-3xl mb-5">Login</button>
+						<button className="manu-button py-3  rounded-3xl">Sign up</button>
+					</ul>
+				</div>
+			</div>
 		</div>
 	);
 };
--- Components/Subscribe/Subscribe.js
+++ Components/Subscribe/Subscribe.js
@@ -4,16 +4,16 @@ import Image from "next/image";
 const Subscribe = () => {
     return (
         <div>
-             <div className="flex flex-col items-center mb-20 ">
-             <Image src="/removebg-preview.png" width={200} height={100}></Image>
-
-
-                <h1 className="font-sans font-normal">Stay up to date with all Jamstack & Deplipy news</h1>
-                <div className=" flex items-center mt-4">
-                <input type="email" placeholder="Email Address" className="input input-bordered px-5 mr-1" />
-                    <button className="manu-button py-3 px-6 rounded-2xl">Subscribe</button>
+            <div className="flex flex-col items-center mb-20 ">
+                <div className="w-20 mb-5 rounded">
+                    <img src="https://i.ibb.co/5GgkG1j/removebg-preview.png" />
                 </div>
-             </div>
+                <h1 className="text-black font-semibold">Stay up to date with all Jamstack & Deplipy news</h1>
+                <div className=" flex items-center mt-4">
+                    <input type="email" name="email" placeholder="Email Address" className="input input-bordered md:pr-32 mr-1" />
+                    <button className="subscribe-button py-3 px-6 rounded-lg">Subscribe</button>
+                </div>
+            </div>
         </div>
     );
 };
