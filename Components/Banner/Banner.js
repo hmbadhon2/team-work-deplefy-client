2fd0f4c31b3bc68c62127303dfbec6527587ece3
Lipton Barua <liptonbarua274@gmail.com>
2023-01-16 14:53:49 +0600

    project

--- Components/Banner/Banner.js
+++ Components/Banner/Banner.js
@@ -5,12 +5,14 @@ const Banner = () => {
     return (
         <div className="hero">
             <div className="hero-content flex-col md:flex-row-reverse">
-            <Image src="/remove.png" width={800} height={800}></Image>
-                <div>
+              <div className="">
+              <Image src="/remove.png" width={700} height={700}></Image>
+              </div>
+                <div className="mr-5">
                     <div className="text-4xl md:text-5xl lg:text-5xl font-bold text-pink-800">
                     <Typewriter
                         options={{
-                            strings: ["Deploy Your Website With Deplify."],
+                            strings: ["Deploy Your Website <br> With Deplify."],
                             autoStart: true,
                             loop: true,
                            
