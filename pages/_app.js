918b5040fb7a58c167872342003480439d487663
RHN007 <rakibul.nayon@outlook.com>
2023-01-18 08:29:37 +0600

    solving error

--- pages/_app.js
+++ pages/_app.js
@@ -7,24 +7,17 @@ import { ToastContainer } from 'react-toastify';
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <div>
- <div className="header lg:px-[108px]">
- <Header></Header>
- </div>
-  <Component {...pageProps} />
-  <div className="bg-black text-white lg:px-[108px]">
-  <Footer></Footer>
-  </div>
-  </div>
-}
-
-
-export default function App({ Component, pageProps }) {
   return <AuthProvider>
-  
-    <Component {...pageProps} />
-    <ToastContainer />
+    <div>
+      <div className="header lg:px-[108px]">
+        <Header></Header>
+      </div>
+      <Component {...pageProps} />
+      <ToastContainer />
+      <div className="bg-black text-white lg:px-[108px]">
+        <Footer></Footer>
+      </div>
+    </div>
   </AuthProvider>
-  
 }
 
