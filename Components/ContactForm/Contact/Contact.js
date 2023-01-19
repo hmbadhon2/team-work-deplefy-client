9e5d6d0652a9cf2ed151122cb765cb1881488ec6
Lipton Barua <liptonbarua274@gmail.com>
2023-01-20 00:41:52 +0600

    project

--- Components/ContactForm/Contact/Contact.js
+++ Components/ContactForm/Contact/Contact.js
@@ -7,7 +7,7 @@ const Contact = () => {
     const socket = io("http://localhost:5000", { transports: ['websocket'] });
     const [message, setMessage] = useState("")
     const [getMessage, setGetMessage] = useState([])
-    const{user}=useContext(AuthContext)
+    const { user } = useContext(AuthContext)
 
 
     const handleSubmit = (e) => {
@@ -26,26 +26,27 @@ const Contact = () => {
     //     })
     // }, [socket])
     return (
-        <div className="grid justify-center">
-            <div className="flex flex-col p-8 shadow-sm rounded-xl lg:p-12 dark:bg-gray-900 dark:text-gray-100">
-                <div className="flex flex-col items-center bg-black">
-                    <div className="border-b-2 w-full text-white text-center py-5 border-white">
+        <div>
+            <section className="p-6 dark:text-gray-400">
+                <div novalidate="" className="container md:w-96 bg-black max-w-xl mx-auto space-y-6 rounded-md shadow dark:bg-gray-900 ng-untouched ng-pristine ng-valid">
+                    <div className="border-b-2 md:w-full text-white text-center py-5 border-white">
                         <h1 className="font-bold">My Chats</h1>
                     </div>
-                    <div className=" p-5">
-                        <div className="text-white overflow-y-auto h-72 sticky">
-                            {
-                                getMessage.map(((p) => (<div>{p.post}</div>)))
-                            }
-                        </div>
-                        <div className="flex">
-                          <input onChange={(e) => setMessage(e.target.value)} type="text" className="w-80 py-2 rounded-l" />
-                          <button onClick={handleSubmit} className="bg-pink-800 px-5 text-white rounded-r">send</button>
+                <div className="p-4">
+                <div className="text-white overflow-y-auto h-72 sticky">
+                        {
+                            getMessage.map(((p) => (<div>{p.post}</div>)))
+                        }
+                    </div>
 
-                        </div>
+                    <div className="flex">
+                        <input onChange={(e) => setMessage(e.target.value)} type="text" className="w-80 py-2 rounded-l" />
+                        <button onClick={handleSubmit} className="bg-pink-800 px-5 text-white rounded-r">send</button>
+
                     </div>
                 </div>
-            </div>
+                </div>
+            </section>
         </div>
     );
 };
