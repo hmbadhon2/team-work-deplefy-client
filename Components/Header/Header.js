f2313d860cccc8b9ed9814ab45d30376c887bb72
Lipton Barua <liptonbarua274@gmail.com>
2023-01-23 23:44:44 +0600

    site complete

--- Components/Header/Header.js
+++ Components/Header/Header.js
@@ -63,13 +63,13 @@ const Header = () => {
 
 					<div>
 						{isOpen ?
-							<label onClick={() => setIsOpen(!isOpen)} tabIndex={0} className="m-1"><button className="">
+							<label onClick={() => setIsOpen(!isOpen)} tabIndex={0} className="m-1"><button className="text-pink-800 font-bold">
 								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
 									<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
 								</svg>
 							</button></label>
 							:
-							<label onClick={() => setIsOpen(!isOpen)} tabIndex={0} className="m-1"><button className="text-pink-900">
+							<label onClick={() => setIsOpen(!isOpen)} tabIndex={0} className="m-1"><button className="text-pink-800 font-bold">
 								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
 									<path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
 								</svg>
