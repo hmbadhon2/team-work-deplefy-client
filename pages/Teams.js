889780b38a9473e32df4beb66e0901124c30eda0
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2023-01-25 02:14:22 +0600

    team settings section add

---
+++ pages/Teams.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Team from '../Components/Team/Team';
+
+const Teams = () => {
+
+
+
+    return (
+        <div className='md:max-w-[1140px] md:mx-auto'>
+            <Team></Team>
+        </div>
+    );
+};
+
+export default Teams;
