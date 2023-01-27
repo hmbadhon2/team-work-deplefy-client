69df10acce09ba471a6c439cc33a84084c06eb07
Lipton Barua <liptonbarua274@gmail.com>
2023-01-28 01:10:49 +0600

    project

--- Components/Header/Header.js
+++ Components/Header/Header.js
@@ -15,7 +15,7 @@ const Header = () => {
 	}, [])
 
 	if (!mounted) return null;
-	const currentTheme = theme === "system" ? systemTheme : theme;
+	// const currentTheme = theme === "system" ? systemTheme : theme;
 
 	const manuItem = <>
 		<li className="">
@@ -50,7 +50,7 @@ const Header = () => {
 	const renderThemeChanger = () => {
 	
 
-		if (currentTheme === "dark") {
+		if (theme === "dark") {
 			return (
 				<div onClick={() => setTheme('light')}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-info">
 					<path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
@@ -97,7 +97,7 @@ const Header = () => {
 						</div>
 						{
 							user?.uid ? <div> {
-								currentTheme === "dark" ? <button onClick={handleLogOut} className="btn btn-outline btn-info px-5 py-2 rounded-3xl mr-5">Log Out</button> : <button onClick={handleLogOut} className=" manu-button px-5 py-2 rounded-3xl mr-5">Log Out</button>
+								theme === "dark" ? <button onClick={handleLogOut} className="btn btn-outline btn-info px-5 py-2 rounded-3xl mr-5">Log Out</button> : <button onClick={handleLogOut} className=" manu-button px-5 py-2 rounded-3xl mr-5">Log Out</button>
 							}
 							</div>
 
