e3a6b853f0235cc252560cae6a7008283e3ffd9b
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2023-02-03 16:02:31 +0600

    modal desing complate

--- Components/UpdateUserModal/UpdateUserModal.js
+++ Components/UpdateUserModal/UpdateUserModal.js
@@ -11,8 +11,22 @@ const UpdateUserModal = () => {
             <div className="modal">
                 <div className="modal-box relative">
                     <label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-                    <h3 className="text-lg font-bold">Congratulations random Internet user!</h3>
-                    <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                    <h3 className="text-lg font-bold">Team Information</h3>
+                
+
+
+                <form action="">
+                <input type="text" placeholder="Name" className="input input-bordered w-full mb-3 mt-8" />
+                <input type="text" placeholder="Email" className="input input-bordered w-full mb-3 " />
+                <input type="text" placeholder="Phone" className="input input-bordered w-full  mb-3" />
+                <input type="text" placeholder="Address" className="input input-bordered w-full mb-3 " />
+                
+
+                <div className='w-full text-center mt-6'>
+                <button type='submit' className="btn w-1/2  btn-sm">Submit</button>
+                </div>
+                </form>
+
                 </div>
             </div>
         </div>
