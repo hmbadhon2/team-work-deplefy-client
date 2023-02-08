f682d9b6f0feb6501405b8bfc5fe87ff7514fa5f
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2023-02-08 17:27:52 +0600

    add new site modal

--- Components/Siter/Siter/Siter.js
+++ Components/Siter/Siter/Siter.js
@@ -12,9 +12,10 @@ const Siter = () => {
                     <div className="dropdown">
                         
                         <label tabIndex={0} className="new-site rounded-lg dark:bg-info dark:text-black m-1 py-16 px-5">Add New Site</label>
-                        <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-white dark:bg-black dark:text-white rounded-box md:w-52 dark:text-black">
-                            <li><a className='font-serif'>Deploy Manually </a></li>
+                        <ul tabIndex={0} className="dropdown-content menu mt-4 p-2 shadow bg-white dark:bg-black dark:text-white rounded-box md:w-52 dark:text-black">
+                        <label htmlFor="my-modal-3" className='font-serif ml-3'>Deploy Manually</label>
                             <li><a className='font-serif'>Tamplate</a></li>
+                           
                         </ul>
                     </div>
         
--- pages/Sites.js
+++ pages/Sites.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import AddSiteModal from '../Components/Siter/Siter/AddSiteModal';
 import Siter from '../Components/Siter/Siter/Siter';
 
 const Sites = () => {
     return (
         <div className='md:max-w-[1140px] md:mx-auto'>
         <Siter></Siter>
+        <AddSiteModal></AddSiteModal>
     </div>
     );
 };
