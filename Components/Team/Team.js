8aee797afd410e4752140565136eb57af5a166b6
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2023-02-10 15:22:18 +0600

    setting page design

--- Components/Team/Team.js
+++ Components/Team/Team.js
@@ -4,6 +4,9 @@ import moment from 'moment/moment';
 import React, { useContext } from 'react';
 import { AuthContext } from '../../context/AuthContext';
 import UpdateUserModal from '../UpdateUserModal/UpdateUserModal';
+import { Player } from '@lottiefiles/react-lottie-player'
+
+
 
 
 const Team = () => {
@@ -23,9 +26,9 @@ const Team = () => {
   })
 
   return (
-    <div className='my-20'>
+    <div className='my-4'>
 
-      <div className='border shadow-lg w-full md:w-1/2 md:px-4 py-7 mb-14  border-gray-300'>
+      <div className='border w-full md:w-1/2 md:px-4 py-7 mb-14  border-gray-300'>
         {teamData[0]?.teamName ? <h1 className=' text-lg md:text-3xl mb-3'>Settings for <span>Register {teamData[0]?.teamName}</span></h1> : <h1 className=' text-lg md:text-3xl mb-3'>Settings for <span>Register team name</span></h1>}
         <h2 className=' text-sm md:text-xl mb-3'>{profileImage?.length ? profileImage?.length : 1} team member</h2>
         <h1 className=' text-sm md:text-xl mb-3'>Team created on {moment(profileImage[0]?.date).format('LL')} ({moment(profileImage[0]?.date).startOf('hour').fromNow()}).</h1>
@@ -39,36 +42,41 @@ const Team = () => {
       {/* Secound step */}
 
 
-      <div className='my-10 md:flex flex-col md:flex-row'>
+      <div className='my-0 md:flex flex-col md:flex-row mb-20'>
 
 
-        <div className=' w-full shadow-lg md:w-1/4 h-48 border border-gray-300 flex md:justify-center md:justify-start'>
+        <div className=' w-full  md:w-1/2  flex  md:justify-start'>
           <ul className='md:ml-6'>
-            <li className='text-gray-500 mb-3 text-lg'>Genaral</li>
-            <div className='ml-2'>
-              <li className='text-gray-500 mb-3'>Team Datails</li>
-              <li className='text-gray-500 mb-3'>Danger zone</li>
-              <li className='text-gray-500 mb-3'>Fast Hosting Services</li>
-              <li className='text-gray-500 mb-3'>Total Security</li>
-            </div>
+            
+            <Player className=' w-full md:w-3/4'
+              autoplay
+              loop
+              src="https://assets10.lottiefiles.com/packages/lf20_z3pnisgt.json"></Player>
+
+
+
+
+
           </ul>
         </div>
 
 
 
-        <div className=' w-full md:w-2/3 border md:ml-6 mt-7 md:mt-0 px-6 py-10 shadow-lg'>
-          <div className='mb-10 py-10'>
-            <h1 className='text-3xl '>Team Details</h1>
-            <h1 >Your team’s profile and single sign-on information</h1>
-          </div>
-
-
+        <div className=' w-full md:w-1/2  md:ml-6 mt-7 md:mt-0 px-6 py-16 '>
+        
 
 
 
           <div className=''>
 
-            <h1 className='text-3xl mb-5'>Team information</h1>
+           <div className='flex justify-start items-center'>
+           <Player className='w-17 h-11'
+              autoplay
+              loop
+              src="https://assets4.lottiefiles.com/packages/lf20_wcdzldes.json"></Player>
+
+             <h1 className='text-3xl mb-5'>Team information</h1>
+           </div>
 
             <div className=" w-full">
               <table className="">
@@ -112,7 +120,7 @@ const Team = () => {
                       </div>
                     </td>
                     <td>
-                    {teamData[0]?.logo?  <Avatar className='ml-20 text-lg font-serif' src={teamData[0]?.logo} alt="avatar" /> :  <Avatar className='ml-20 text-lg font-serif' src="https://png.pngtree.com/png-clipart/20190520/original/pngtree-business-male-icon-vector-png-image_4187852.jpg" alt="avatar" />}
+                      {teamData[0]?.logo ? <Avatar className='ml-20 text-lg font-serif' src={teamData[0]?.logo} alt="avatar" /> : <Avatar className='ml-20 text-lg font-serif' src="https://png.pngtree.com/png-clipart/20190520/original/pngtree-business-male-icon-vector-png-image_4187852.jpg" alt="avatar" />}
                     </td>
                   </tr>
 
@@ -137,7 +145,7 @@ const Team = () => {
 
             {/* <button className="builds-button px-4 py-2 rounded-lg font-semibold dark:bg-info dark:text-black mt-10">Edit Team Information</button> */}
 
-            <label htmlFor="my-modal-3" className="builds-button px-4 py-2 rounded-lg font-semibold dark:bg-info dark:text-black">Edit Team Information</label>
+            <label htmlFor="my-modal-3" className="builds-button px-4 py-2 rounded-lg font-semibold dark:bg-info dark:text-black ">Edit Team Information</label>
           </div>
 
         </div>
