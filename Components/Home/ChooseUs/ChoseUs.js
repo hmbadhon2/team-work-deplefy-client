7f8f12dc584e4c3ed112e0c4b0e318c152f03c03
hmbadhon2 <hmmizanurrahmanbadhon@gmail.com>
2023-02-13 22:28:38 +0600

    try to deploy problem solved

--- Components/Home/ChooseUs/ChoseUs.js
+++ Components/Home/ChooseUs/ChoseUs.js
@@ -11,17 +11,17 @@ const ChoseUs = () => {
 
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 py-12 px-12 bg-gray-100 rounded text-black">
                     <div className=" flex items-center flex-col rounded border border-gary-200 p-10 text-center hover:bg-white">
-                        <Image src="/Choose us Logo/Best Price Logo.png" height={80} width={80}></Image>
+                        <Image src="/Choose us Logo/Best Price Logo.png" height={80} width={80} alt="price logo"></Image>
                         <h3 className="font-semibold text-3xl my-10">Best Pricing Deals</h3>
                         <p className="leading-8 font-semibold text-lg">We provide the best hosting price. Also, we offer a 14-day money-back guarantee.</p>
                     </div>
                     <div className=" flex items-center flex-col rounded border border-gary-200 p-10 text-center hover:bg-white">
-                        <Image src="/Choose us Logo/Easy use logo.png" height={80} width={80}></Image>
+                        <Image src="/Choose us Logo/Easy use logo.png" height={80} width={80} alt="price logo"></Image>
                         <h3 className="font-semibold text-3xl my-10">Super Easy To Use</h3>
                         <p className="leading-8 font-semibold text-lg">Within a few clicks, you can set up your website hosting and environment easily.</p>
                     </div>
                     <div className=" flex items-center flex-col rounded border border-gary-200 p-10 text-center hover:bg-white">
-                        <Image src="/Choose us Logo/Support logo.png" height={80} width={80}></Image>
+                        <Image src="/Choose us Logo/Support logo.png" height={80} width={80} alt="price logo" ></Image>
                         <h3 className="font-semibold text-3xl my-10">Dedicated Support</h3>
                         <p className="leading-8 font-semibold text-lg">We support all our customers 24/7, so they can manage their websites smoothly.</p>
                     </div>
