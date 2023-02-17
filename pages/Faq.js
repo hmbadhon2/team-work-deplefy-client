2bd52b4499aefc7a55fcaa4bfdc076a53c07e0de
milonahmedshuvo <milonahmedshuvo01@gmail.com>
2023-02-17 10:57:02 +0600

    FAQ camming soon

---
+++ pages/Faq.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HostingFaq from '../Components/HostingFaq/HostingFaq';
+
+const Faq = () => {
+    return (
+        <div>
+            <HostingFaq></HostingFaq>
+        </div>
+    );
+};
+
+export default Faq;
