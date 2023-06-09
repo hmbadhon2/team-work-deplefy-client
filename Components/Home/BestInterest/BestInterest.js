d0508dc249e546e98075ea08c9a8a6ab87e41fe6
Lipton Barua <liptonbarua274@gmail.com>
2023-06-09 19:00:07 +0600

    project

--- Components/Home/BestInterest/BestInterest.js
+++ Components/Home/BestInterest/BestInterest.js
@@ -8,17 +8,17 @@ const BestInterest = () => {
                     <div className=" flex items-center flex-col rounded p-10 text-center hover:bg-white dark:hover:bg-[#161111]" style={{transition: `2s`}}>
                         <HiCubeTransparent className='w-16 h-16'></HiCubeTransparent>
                         <h3 className="font-semibold text-2xl mb-4">Actine in Your Best Interest </h3> 
-                        <p className="leading-8 font-semibold text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus ut similique fugiat eaque debitis. Rerum nisi culpa repellat sed adipisci..</p>
+                        <p className="text-justify leading-8 text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus ut similique fugiat eaque debitis. Rerum nisi culpa repellat sed adipisci..</p>
                     </div>
                     <div className=" flex items-center flex-col rounded p-10 text-center hover:bg-white dark:hover:bg-[#161111]" style={{transition: `2s`}}>
                        <HiStatusOnline className='w-16 h-16'></HiStatusOnline>
                         <h3 className="font-semibold text-2xl mb-4">Planning For the Future </h3> 
-                        <p className="leading-8 font-semibold text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus ut similique fugiat eaque debitis. Rerum nisi culpa repellat sed adipisci..</p>
+                        <p className="text-justify leading-8 text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus ut similique fugiat eaque debitis. Rerum nisi culpa repellat sed adipisci..</p>
                     </div>
                     <div className=" flex items-center flex-col rounded p-10 text-center hover:bg-white dark:hover:bg-[#161111]" style={{transition: `2s`}}>
                         <HiOutlineFire className='w-16 h-16'></HiOutlineFire>
                         <h3 className="font-semibold text-2xl mb-4">Customer Value Chain </h3> 
-                        <p className="leading-8 font-semibold text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus ut similique fugiat eaque debitis. Rerum nisi culpa repellat sed adipisci..</p>
+                        <p className="text-justify leading-8 text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus ut similique fugiat eaque debitis. Rerum nisi culpa repellat sed adipisci..</p>
                     </div>
              
             </div>
