ee01a48e0964d7c3ee941499e60b8551d8a1b33d
unknown <liptonbarua274@gmail.com>
2023-06-15 13:14:41 +0600

    project end

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className='bg-indigo-100  dark:bg-gradient-to-tr dark:from-  dark:to-black'>
+      <body className='bg-indigo-100  dark:bg-gradient-to-tr dark:from-black  dark:to-black'>
         <Main />
         <NextScript />
       </body>
